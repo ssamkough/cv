2f661e9b8919c9bdffaa181c74c7c2b1e20b9648
Sammy Samkough <sammysamkough@hotmail.com>
2022-10-26 22:42:52 -0400

    updated consulting

--- src/components/detailed-career-item.js
+++ src/components/detailed-career-item.js
@@ -11,7 +11,7 @@ template.innerHTML = `
     <header>
       <section>
         <div>
-         <strong class="company"></strong> | <strong class="role-title"></strong>
+         <span class="company"></span><strong class="role-title"></strong>
         </div>
       </section>
       <section>
@@ -22,25 +22,28 @@ template.innerHTML = `
     </header>
     <main>
       <div><slot name="description" /></div>
-      <div><u>technologies</u>: <span class="technologies"></span></div>
+      <div><u class="technologies-underline"></u><span class="technologies"></span></div>
     </main>
   </section>
 `;
 
 class DetailedCareerItem extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
 
-        this.shadowRoot.querySelector('.company').innerText = this.getAttribute('company');
-        this.shadowRoot.querySelector('.role-title').innerText = this.getAttribute('role-title');
-        this.shadowRoot.querySelector('.start-month').innerText = this.getAttribute('start-month');
-        this.shadowRoot.querySelector('.start-year').innerText = this.getAttribute('start-year');
-        this.shadowRoot.querySelector('.end-month').innerText = this.getAttribute('end-month') || 'present';
-        this.shadowRoot.querySelector('.end-year').innerText = this.getAttribute('end-year');
-        this.shadowRoot.querySelector('.technologies').innerText = this.getAttribute('technologies');
-    }
+    this.shadowRoot.querySelector('.company').innerText = this.getAttribute('company');
+    this.shadowRoot.querySelector('.company').style.fontWeight = this.getAttribute('role-title') ? '800' : '500';
+    this.shadowRoot.querySelector('.company').style.fontStyle = this.getAttribute('role-title') ? 'none' : 'italic';
+    this.shadowRoot.querySelector('.role-title').innerText = this.getAttribute('role-title') ? " | " + this.getAttribute('role-title') : "";
+    this.shadowRoot.querySelector('.start-month').innerText = this.getAttribute('start-month');
+    this.shadowRoot.querySelector('.start-year').innerText = this.getAttribute('start-year');
+    this.shadowRoot.querySelector('.end-month').innerText = this.getAttribute('end-month') || 'present';
+    this.shadowRoot.querySelector('.end-year').innerText = this.getAttribute('end-year');
+    this.shadowRoot.querySelector('.technologies').innerText = this.getAttribute('technologies') ? ': ' + this.getAttribute('technologies') : '';
+    this.shadowRoot.querySelector('.technologies-underline').innerText = this.getAttribute('technologies') ? 'technologies' : '';
+  }
 }
 
 customElements.define('detailed-career-item', DetailedCareerItem);
